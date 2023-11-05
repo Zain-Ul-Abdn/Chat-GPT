9800553502d75ad88be4ddbc5bc02bdaed410289
Zain_@Developer <zainmehboob376@gmail.com>
2023-11-05 23:16:52 +0500

    Adding SidePanel component

--- src/Assets/components/Header.js
+++ src/Assets/components/Header.js
@@ -4,10 +4,18 @@ import darkPanel from '../Icons/dark-panel.png'
 import sunlogo from '../Icons/sun.png'
 import moonlogo from '../Icons/moon.png'
 import lightPanel from '../Icons/light-panel.png'
-
+import Sidepanel from './Sidepanel'
 
 function Header(props) {
     const [check, setCheck] = useState(false);
+
+    const [checkPanel, setCheckPanel] = useState(false);
+    const [showpanel, setShowpanel] = useState({
+        'position': 'absolute',
+        'top': '0',
+        'left': '-280px'
+    })
+
 
     const [mode, setMode] = useState({
         'color': 'Black',
@@ -35,17 +43,48 @@ function Header(props) {
             })
             setDaymode(sunlogo)
             setPanelmode(lightPanel)
-            
+
         }
     }
 
+    
+    function handleclick() {
+
+        if (checkPanel) {
+            setShowpanel({
+                'position': 'absolute',
+                'top': '0',
+                'left': '0'
+            })
+            setCheckPanel(false)
+            setPanelmode(lightPanel)
+            
+        }
+        else {
+            setShowpanel({
+                'position': 'absolute',
+                'top': '0',
+                'left': '-280px'
+            })
+            setCheckPanel(true)
+            setPanelmode(darkPanel)
+
+        }
+
+    }
+
     return (
-        <div className='navbar' style={mode}>
+        <>
             <input type="checkbox" checked={check} />
-            <img className='sidePanellogo' src={panelmode} />
-            <p style={mode}>{props.name}</p>
-            <img className='darkMode' src={daymode} onClick={changeMode} />
-        </div>
+            <input type="checkbox" checked={checkPanel} />
+
+            <div className='navbar' style={mode}>
+                <img className='sidePanellogo' src={panelmode} onClick={handleclick} />
+                <p style={mode}>{props.name}</p>
+                <img className='darkMode' src={daymode} onClick={changeMode} />
+            </div>
+            <Sidepanel style={showpanel} />
+        </>
     )
 }
 
---
+++ src/Assets/components/Sidepanel.js
@@ -0,0 +1,10 @@
+import '../components/Sidepanel.css'
+function Sidepanel(props) {
+  return (
+    <div className="container" style={props.style}>
+
+    </div>
+  )
+}
+
+export default Sidepanel
